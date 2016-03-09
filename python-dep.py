95c228178b8ec450d690fd99a2af1b9478a7dc74
Saito Tsutomu(KKE) <tsutomu@kke.co.jp>
2016-03-09 22:35:37 +0900

    a

--- python-dep.py
+++ python-dep.py
@@ -1,8 +1,9 @@
 import graphviz
-from subprocess import run, PIPE
-ss = run(['pipdeptree'], stderr=PIPE, universal_newlines=True).stderr.rstrip().split('\n')
-ss = [s[2:].split()[:3] for s in ss if s[0] in ' *']
-g = graphviz.Digraph(format='png', filename='python-dep')
-g.edges([(s[0][:s[0].index('=')], s[2]) for s in ss])
-g.engine = 'fdp'
+from subprocess import run, DEVNULL, PIPE
+ss = run(['pipdeptree'], stdout=DEVNULL, stderr=PIPE, 
+         universal_newlines=True).stderr.rstrip().split('\n')
+ss = [s[2:].lower().split()[:3] for s in ss if s[0] in ' *']
+g = graphviz.Digraph(format='png', filename='python-dep', engine='dot')            
+g.edges([(s[2], s[0][:s[0].index('=')]) for s in ss])
+g.attr('graph', rankdir='LR')
 g.render()
